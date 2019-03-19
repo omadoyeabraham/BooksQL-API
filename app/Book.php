1eae6bc8c56901a22e5b92431a4662c5a433b34b
omadoyeabraham <omadoyeabraham@gmail.com>
2019-03-19 06:53:50 +0100

    [development] Created models and migrations for Books and Categories

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function category()
+    {
+        return $this->BelongsTo(Category::class);
+    }
 }
